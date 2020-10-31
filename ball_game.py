99d69f2e8c92ae8d046d150da4b973f8a15b915c
Pavel Gurevich <gurevich.pe@phystech.edu>
2020-10-31 23:08:38 +0300

    за круги и квадраты разное кол-во очков, автоматически записывает результат в файл и сортирует результаты по убыванию. Квадраты в целом меньше кругов и двигаются быстрее

--- ball_game.py
+++ ball_game.py
@@ -19,10 +19,14 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-x = random.randint(0, WIDTH)
-y = random.randint(0, HEIGHT)
+
+name = input('Enter user`s name')
 
 font_name = pygame.font.match_font('arial')
+
+
+def table_top(i):
+    return int(i[1])
 
 def draw_text(surf, text, size, x, y):
     font = pygame.font.Font(font_name, size)
@@ -31,11 +35,35 @@ def draw_text(surf, text, size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
-class Player(pygame.sprite.Sprite):
+def record_table(name, score):
+    f = open('records.txt', 'a')
+    f.write(name + ' ' + str(score) + '\n')
+    f.close()
+
+def sort_table():
+    f = open('records.txt', 'r')
+    results = []
+    while True:
+        res = f.readline()
+        if res == '':
+            break
+        results.append(res)
+
+    for i in range(len(results)):
+        results[i] = results[i].split()
+
+    f.close()
+    fw = open('records.txt', 'w')
+    results.sort(key=table_top, reverse=True)
+    for i in range(len(results)):
+        fw.write(str(results[i][0]) + " " + str(results[i][1]) + "\n")
+    fw.close()
+
+class Ball(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        a = random.randint(20, 100)
+        a = random.randint(30, 100)
         self.image = pygame.Surface((a, a))
         self.image.set_colorkey(BLACK)
         self.rad = a//2
@@ -59,6 +87,33 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < 0:
             self.speedy = random.randint(1, 10)
 
+class Square(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        a = random.randint(20, 80)
+        self.width = a
+        self.image = pygame.Surface((a, a))
+        self.image.set_colorkey(BLACK)
+        color = COLORS[random.randint(0, 5)]
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(100, 700), random.randint(100, 500))
+        self.speedx = random.randint(5, 15)
+        self.speedy = random.randint(-15, 15)
+
+    def update(self):
+
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.right > WIDTH:
+            self.speedx = random.randint(-15, 1)
+        if self.rect.left < 0:
+            self.speedx = random.randint(1, 15)
+        if self.rect.bottom > HEIGHT:
+            self.speedy = random.randint(-15, -1)
+        if self.rect.top < 0:
+            self.speedy = random.randint(1, 15)
 
 
 
@@ -71,8 +126,11 @@ clock = pygame.time.Clock()
 
 
 all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+balls = pygame.sprite.Group()
+squares = pygame.sprite.Group()
+ball = Ball()
+balls.add(ball)
+all_sprites.add(ball)
 
 
 # Цикл игры
@@ -84,20 +142,33 @@ while running:
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
+            record_table(name, score)
+            sort_table()
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            for sprite in all_sprites:
+            for sprite in balls:
                 if sqrt((sprite.rect.center[0] - pos[0])**2 + (pos[1] - sprite.rect.center[1])**2) < sprite.rad:
                     sprite.kill()
                     score += 1
+
+            for sprite in squares:
+                if sprite.rect.right > pos[0] > sprite.rect.x and sprite.rect.bottom > pos[1] > sprite.rect.y:
+                    sprite.kill()
+                    score += 2
+
 
 
     # Обновление
     time += 1
     if time%40 == 0:
-        player = Player()
-        all_sprites.add(player)
+        ball = Ball()
+        balls.add(ball)
+        all_sprites.add(ball)
+    elif time%50 == 0:
+        sq = Square()
+        squares.add(sq)
+        all_sprites.add(sq)
     all_sprites.update()
 
 
